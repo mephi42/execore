7cf240c2678e6d54c569f1519274c9073c237a35
mephi42 <mephi42@gmail.com>
2023-05-05 23:34:17 +0200

    Allow passing extra arguments for GDB
    Closes #2.

--- src/execore_string.h
+++ src/execore_string.h
@@ -10,4 +10,12 @@ static __attribute__((unused)) void *memchr(const void *s, int c, size_t n) {
   return NULL;
 }
 
+#define strdupa(s)                                                             \
+  (__extension__({                                                             \
+    const char *__old = (s);                                                   \
+    size_t __len = strlen(__old) + 1;                                          \
+    char *__new = (char *)__builtin_alloca(__len);                             \
+    (char *)memcpy(__new, __old, __len);                                       \
+  }))
+
 #endif
