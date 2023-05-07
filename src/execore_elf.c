373f576ee4a2061cfa37701b5f555f80c694552a
mephi42 <mephi42@gmail.com>
2023-05-07 18:44:06 +0200

    Map NT_FILE entries
    Closes #13.

--- src/execore_elf.c
+++ src/execore_elf.c
@@ -21,15 +21,15 @@ static off_t for_each_note_1(const char *path, int fd, off_t off,
 
   char *name = alloca(name_sz);
   PREAD_EXACT(path, fd, name, name_sz, name_off, err);
-  char *desc = alloca(desc_sz);
-  PREAD_EXACT(path, fd, desc, desc_sz, desc_off, err);
   struct note note = {
       .type = note_hdr[2],
       .name = name,
-      .desc = desc,
-      .desc_end = desc + desc_sz,
+      .desc_off = desc_off,
+      .desc_sz = desc_sz,
   };
-  return cb(&note, arg);
+  if (cb(&note, arg) == -1)
+    goto err;
+  return next_off;
 
 err:
   return -1;
@@ -56,6 +56,45 @@ int for_each_note(const char *path, int fd, Elf64_Ehdr *ehdr,
       return 0;
     }
   }
+
+err:
+  return -1;
+}
+
+int for_each_nt_file(const char *path, int fd, struct note *n,
+                     int (*cb)(struct nt_file *, void *), void *arg) {
+  unsigned long hdr[2];
+  PREAD_EXACT(path, fd, hdr, sizeof(hdr), n->desc_off, err);
+  unsigned long count = hdr[0];
+  unsigned long page_size = hdr[1];
+  off_t file_off = n->desc_off + sizeof(hdr);
+  off_t filename_off = file_off + sizeof(unsigned long) * 3 * count;
+  for (unsigned long i = 0; i < count; i++) {
+    unsigned long file[3];
+    PREAD_EXACT(path, fd, file, sizeof(file), file_off, err);
+    char filename[PATH_MAX + 1];
+    size_t pread_count = n->desc_sz - (filename_off - n->desc_off);
+    if (pread_count > sizeof(filename))
+      pread_count = sizeof(filename);
+    PREAD_EXACT(path, fd, filename, pread_count, filename_off, err);
+    size_t filename_len = strnlen(filename, pread_count - 1);
+    if (filename[filename_len] != 0) {
+      fprintf(stderr,
+              "%s contains NT_FILE with a file name which is too long\n", path);
+      goto err;
+    }
+    struct nt_file f = {
+        .start = file[0],
+        .end = file[1],
+        .offset = file[2] * page_size,
+        .filename = filename,
+    };
+    if (cb(&f, arg) == -1)
+      goto err;
+    file_off += sizeof(unsigned long) * 3;
+    filename_off += filename_len + 1;
+  }
+  return 0;
 
 err:
   return -1;
--- src/execore_elf.h
+++ src/execore_elf.h
@@ -6,11 +6,21 @@
 struct note {
   unsigned int type;
   const char *name;
-  const char *desc;
-  const char *desc_end;
+  off_t desc_off;
+  unsigned int desc_sz;
 };
 
 int for_each_note(const char *path, int fd, Elf64_Ehdr *ehdr,
                   int (*cb)(struct note *, void *), void *arg);
+
+struct nt_file {
+  unsigned long start;
+  unsigned long end;
+  unsigned long offset;
+  const char *filename;
+};
+
+int for_each_nt_file(const char *path, int fd, struct note *n,
+                     int (*cb)(struct nt_file *, void *), void *arg);
 
 #endif
