f7d070d5b9544a822e08e23a37d5886e625f61eb
mephi42 <mephi42@gmail.com>
2023-12-15 21:14:52 +0100

    Prepend sysroot to AT_EXECFN
    Closes #79.

--- src/execore_string.h
+++ src/execore_string.h
@@ -19,4 +19,16 @@ static __attribute__((unused)) void *EXECORE_(memchr)(const void *s, int c,
     (char *)memcpy(__new, __old, __len);                                       \
   }))
 
+#define strcata(s1, s2)                                                        \
+  (__extension__({                                                             \
+    const char *__s1 = (s1);                                                   \
+    const char *__s2 = (s2);                                                   \
+    size_t __len1 = strlen(__s1);                                              \
+    size_t __len2 = strlen(__s2);                                              \
+    char *__buf = (char *)__builtin_alloca(__len1 + __len2 + 1);               \
+    memcpy(__buf, __s1, __len1);                                               \
+    memcpy(__buf + __len1, __s2, __len2 + 1);                                  \
+    __buf;                                                                     \
+  }))
+
 #endif
